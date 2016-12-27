ac2f8b9e23d6435ff1c898ce5c4fd0c49f8c8b59
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2016-12-27 13:40:55 -0800

    fixed show nav and added app.js to handle spotify query

---
+++ app.js
@@ -0,0 +1,12 @@
+// Client ID:  bfb932f8e81a4806b0d1a06d9cdadb8a
+
+// Search for Spotify ID based on query string
+// GET https://api.spotify.com/v1/search
+// curl -X GET "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist"
+
+// Get an artists top tracks
+// GET https://api.spotify.com/v1/artists/{id}/top-tracks
+
+// Get Audio Features for multiple Tracks - Requires Spotify ID
+// GET https://api.spotify.com/v1/audio-features
+
---
+++ js/app.js
@@ -0,0 +1,6 @@
+Client ID:  bfb932f8e81a4806b0d1a06d9cdadb8a
+Client Secret:  fadbd326e2784d728dc61e75d182fb36
+
+Get Audio Features for a Track
+GET https://api.spotify.com/v1/audio-features/{id}
+
