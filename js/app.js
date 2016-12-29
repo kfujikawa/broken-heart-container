61da4f68caa7ace35e2c9f4a0e1407daaa8d8cb4
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2016-12-29 15:57:10 -0800

    using popularity as a placeholder to get hearts to display based on score (0-100)

--- js/app.js
+++ js/app.js
@@ -2,11 +2,10 @@
 // curl -X GET "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist"
 var SPOTIFY_SEARCH_URL = "https://api.spotify.com/v1/search";
 
-// Get an artists top tracks
-var SPOTIFY_ARTIST_TOP_TRACKS_URL = "https://api.spotify.com/v1/artists/spotifyId/top-tracks";
-
 // Get Audio Features for multiple Tracks - Requires Spotify ID
 var SPOTIFY_AUDIO_FEATURES_URL = "https://api.spotify.com/v1/audio-features";
+
+var SPOTIFY_AUTHORIZATION_URL = "https://accounts.spotify.com/authorize"
 
 function getSearchDataFromApi(searchTerm, callback){
 	var settings = {
@@ -40,10 +39,38 @@ function storeSpotifyId(data){
 	var dataArray = data.artists;
 	var firstArtistResult = dataArray.items[0];
 	var spotifyId = firstArtistResult.id;
-	console.log("this is the spotify id: " + spotifyId);
-
 	getArtistTopTracksFromApi(spotifyId, displaySpotifyTopTracksData);
 }
+
+// function authorize(){
+// 	var settings = {
+// 		url: SPOTIFY_AUTHORIZATION_URL,
+// 		data: {
+// 			client_id: ,
+// 			response_type: "token",
+// 			redirect_uri: "http://localhost:8888/callback",
+// 		}
+// 	}
+// }
+
+// function getAudioFeaturesFromApi(ids, authorization){
+// 	authorize();
+
+// 	$.ajax({
+// 		url: 'https://api.spotify.com/v1/me',
+// 		headers: {
+// 			'Authorization': 'Bearer ' + accessToken
+// 	},
+// 	   success: function(response){
+// 			var settings = {
+// 				url: SPOTIFY_AUDIO_FEATURES_URL,
+// 				data: {
+// 					authorization: ""
+// 				}
+// 			}
+// 	   }
+// 	});
+// }
 
 // Render Functionality
 function displaySpotifySearchArtistData(data){
@@ -57,8 +84,7 @@ function displaySpotifySearchArtistData(data){
 		var artistName = firstArtistResult.name;
 		var spotifyId = firstArtistResult.id;
 
-		resultElement += "<h3>" + "Artist: " + artistName + "</h3>" +
-						"<p>" + spotifyId + "</p>";
+		resultElement += "<h3>" + "Artist: " + artistName + "</h3>";
 	}
 	
 	else{
@@ -70,17 +96,49 @@ function displaySpotifySearchArtistData(data){
 
 function displaySpotifyTopTracksData(tracks){
 	var trackArray = tracks.tracks;
-	console.log(trackArray);
-
 	var resultElement = "";
-
+	var trackIdArray = [];
+	var popularityArray = [];
 
 	if(trackArray){
 		trackArray.forEach(function(element){
 			var songName = element.name;
+			var trackId = element.id;
+			var popularity = element.popularity;
+			var popularityImage = "";
 
-			resultElement += "<p>" + songName + "</p>"
+			switch (true){
+				case (popularity >= 90 && popularity <= 100):
+					popularityImage = "heart_container_90_100.png";
+					break;
+				case (popularity >= 75 && popularity <= 89):
+					popularityImage = "heart_container_75_89.png";
+					break;
+				case (popularity >= "60"):
+					popularityImage = "heart_container_60_74.png";
+					break;
+				case (popularity >= 45 && popularity <= 59):
+					popularityImage = "heart_container_45_59.png";
+					break;
+				case (popularity >= 30 && popularity <= 44):
+					popularityImage = "heart_container_30_44.png";
+					break;
+				case (popularity >= 15 && popularity <= 29):
+					popularityImage = "heart_container_15_29.png";
+					break;
+				case (popularity >= 0 && popularity <= 14):
+					popularityImage = "heart_container_0_14.png";
+					break;
+			}
+
+			console.log(popularity);
+
+			trackIdArray.push(trackId);
+			popularityArray.push(popularity);
+
+			resultElement += "<p>" + songName + " " +"<img src=" + "img/" + (popularityImage) + ">" + "</p>"
 		});
+
 	}
 	$(".jsTracksSearchResults").html(resultElement);
 }
@@ -92,7 +150,6 @@ function watchSubmit(){
 		var query = $(this).find(".jsQuery").val();
 		getSearchDataFromApi(query, displaySpotifySearchArtistData);
 		getSearchDataFromApi(query, storeSpotifyId);
-
 	});
 }
 
